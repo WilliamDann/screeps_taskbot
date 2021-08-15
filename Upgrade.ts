3b4cb022c330dd0e3b2edd3868b4daa0be247134
WilliamDann <s-william.dann@lwtech.edu>
2021-08-15 05:21:21 -0700

    updated to use memPath

--- Upgrade.ts
+++ Upgrade.ts
@@ -6,18 +6,20 @@ export default class UpgradeProducer extends Producer
     controller  : StructureController;    
     expireTicks : number = 25;
 
+    memPath     : string;
+
     constructor(controller: StructureController)
     {
         super();
         this.controller = controller;
 
-        if (!this.controller.room.memory['lastUpgradeOrder'])
-            this.controller.room.memory['lastUpgradeOrder'] = -1;
+        if (!this.controller.room.memory[this.memPath])
+            this.controller.room.memory[this.memPath] = -1;
     }
 
     private noOrderIssued(): boolean
     {
-        return this.controller.room.memory['lastUpgradeOrder'] == -1;
+        return this.controller.room.memory[this.memPath] == -1;
     }
 
     private issuedOrderHasExpired() : boolean
@@ -25,14 +27,14 @@ export default class UpgradeProducer extends Producer
         if (this.noOrderIssued()) 
             return true;
 
-        return (Game.time - this.controller.room.memory['lastUpgradeOrder']) >= this.expireTicks;
+        return (Game.time - this.controller.room.memory[this.memPath]) >= this.expireTicks;
     }
 
     produce(): Event[]
     {
         if (this.issuedOrderHasExpired())
         {
-            this.controller.room.memory['lastUpgradeOrder'] = Game.time;
+            this.controller.room.memory[this.memPath] = Game.time;
 
             return [ new Event('upgrade', { target: this.controller.id }, Game.time+this.expireTicks) ]
         }
